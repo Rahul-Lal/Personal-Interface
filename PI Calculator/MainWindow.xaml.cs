adcd2875d660fabe8489fa3c1c08ba1d9b4f81f8
Rahul Lal <rahul21lal@hotmail.com>
2017-05-09 01:12:24 +1200

    Start from scratch
    WPF application, create main page and calculator page

---
+++ PI Calculator/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.Foundation;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PI_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculator_Click(object sender, RoutedEventArgs e)
+        {
+            // Frame.Navigate(typeof(Calculator));
+        }
+    }
+}
